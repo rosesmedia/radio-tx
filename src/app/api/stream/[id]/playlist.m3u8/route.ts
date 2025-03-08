6a906b977d39a3a76d51669c72f9788e7bf6fd3d
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-03-08 14:36:27 +0000

    feat: initial working streaming implementation

--- src/app/api/stream/[id]/playlist.m3u8/route.ts
+++ src/app/api/stream/[id]/playlist.m3u8/route.ts
@@ -1,11 +1,15 @@
 import { getHandler } from "@/lib/handlers";
 import { prisma } from "@/lib/db";
-import { NextResponse } from "next/server";
+
+const BASE_URL = 'http://localhost:3001/hls/';
 
 export const GET = getHandler(async ({ id }: { id: string }) => {
     const stream = await prisma.stream.findFirstOrThrow({
         where: {
             fixtureId: id,
+            state: {
+                in: ['Live', 'Complete'],
+            },
         },
         include: {
             HlsSegment: {
@@ -17,14 +21,20 @@ export const GET = getHandler(async ({ id }: { id: string }) => {
                 },
             },
         },
-    })
+    });
 
     const resp = [];
     resp.push('#EXTM3U');
     resp.push('#EXT-X-TARGETDURATION:4');
     resp.push('#EXT-X-VERSION:3');
     resp.push('#EXT-X-MEDIA-SEQUENCE:212');
-    const entries = stream.HlsSegment.map(segment => `#EXTINF:${segment.duration},\n${segment.filename}`);
+    resp.push('#EXT-X-PLAYLIST-TYPE:EVENT');
+    const entries = stream.HlsSegment.map(segment => `#EXTINF:${segment.duration},\n${BASE_URL}${segment.filename}`);
+
+    if (stream.state === 'Complete') {
+        entries.push('#EXT-X-ENDLIST');
+    }
+
     const playlist = [...resp, ...entries].join('\n');
     return new Response(playlist, {
         headers: {
