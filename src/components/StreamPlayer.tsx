13718c08d28c0658d9f4c855852be236b1645fa5
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-04-19 15:22:24 +0100

    feat: some debugging stuff for StreamPlayer

--- src/components/StreamPlayer.tsx
+++ src/components/StreamPlayer.tsx
@@ -68,6 +68,13 @@ export default function StreamPlayer(props: Props) {
   }
 }
 
+function logEvent<T>(event: string, action: (t: T) => void): (t: T) => void {
+  return (t) => {
+    console.log(`[player] [event] ${event}`);
+    action(t);
+  };
+}
+
 function StreamPlayerInner({ streamId, isLive }: Props) {
   const audio = useRef<HTMLAudioElement>(null);
   const [isPaused, setIsPaused] = useState(true);
@@ -80,8 +87,10 @@ function StreamPlayerInner({ streamId, isLive }: Props) {
   useEffect(() => {
     if (!audio.current) return;
     if (audio.current.canPlayType(HLS_MIME)) {
+      console.log('[player] using browser built-in HLS');
       audio.current.src = streamUrl;
     } else if (Hls.isSupported()) {
+      console.log('[player] using hls.js');
       const hls = new Hls({
         // we need to use the worker to avoid this until we can fix the liquidsoap script
         // https://github.com/savonet/liquidsoap/issues/4398
@@ -128,13 +137,13 @@ function StreamPlayerInner({ streamId, isLive }: Props) {
     <Stack>
       <audio
         ref={audio}
-        onPlay={() => setIsPaused(false)}
-        onPause={() => setIsPaused(true)}
-        onStalled={() => setLoading(true)}
-        onPlaying={() => setLoading(false)}
-        onCanPlay={() => setLoading(false)}
-        onTimeUpdate={(e) => setCurrentTime(e.currentTarget.currentTime)}
-        onDurationChange={(e) => setDuration(e.currentTarget.duration)}
+        onPlay={logEvent('onPlay', () => setIsPaused(false))}
+        onPause={logEvent('onPause', () => setIsPaused(true))}
+        onStalled={logEvent('onStalled', () => setLoading(true))}
+        onPlaying={logEvent('onPlaying', () => setLoading(false))}
+        onCanPlay={logEvent('onCanPlay', () => setLoading(false))}
+        onTimeUpdate={logEvent('onTimeUpdate', (e) => setCurrentTime(e.currentTarget.currentTime))}
+        onDurationChange={logEvent('onDurationChange', (e) => setDuration(e.currentTarget.duration))}
       />
 
       <Group>
@@ -176,7 +185,7 @@ function StreamPlayerInner({ streamId, isLive }: Props) {
           size={64}
           aria-label={audio?.current?.paused === false ? 'play' : 'pause'}
           onClick={() => togglePause()}
-          disabled={loading}
+          // disabled={loading}
         >
           {loading ? (
             <Loader color="white" />
