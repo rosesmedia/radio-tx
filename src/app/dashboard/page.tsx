a0951550f79840054f2776b199d7da4bcf1c5c45
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-04-19 16:35:23 +0100

    Revert "log that event"
    i committed entirely the wrong thing
    
    This reverts commit ec19b0b949f5389c5d5e5693e3cddb33c6fe3597.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,9 @@
-import { Button, ButtonGroup, Title } from '@mantine/core';
+import { Title } from '@mantine/core';
 
 export default async function DashboardPage() {
   return (
     <>
       <Title order={1}>Hello!</Title>
-
-      <ButtonGroup>
-        <Button>OBR</Button>
-        <Button variant="default">STN</Button>
-        <Button variant="default">TDF</Button>
-        <Button variant="default">END</Button>
-      </ButtonGroup>
     </>
   );
 }
