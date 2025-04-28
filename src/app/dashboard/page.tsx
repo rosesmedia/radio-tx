c3c6a1454aaff819938cabc31008aafd714fefda
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-04-28 23:31:18 +0100

    feat: live stream counter on dashboard home lol

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,33 @@
-import { Title } from '@mantine/core';
+import { prisma } from '@/lib/db';
+import {
+  Card,
+  Center,
+  Group,
+  SimpleGrid,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
+import { IconAccessPoint } from '@tabler/icons-react';
 
 export default async function DashboardPage() {
+  const liveStreams = await prisma.stream.count({ where: { state: 'Live' } });
+
   return (
-    <>
+    <Stack>
       <Title order={1}>Hello!</Title>
-    </>
+
+      <SimpleGrid cols={{ base: 1, sm: 2, lg: 4 }} spacing="xl">
+        <Card shadow="sm" withBorder>
+          <Group>
+            <Center>
+              <IconAccessPoint size={40} color="red" />
+            </Center>
+
+            <Title order={2}>Live streams: {liveStreams}</Title>
+          </Group>
+        </Card>
+      </SimpleGrid>
+    </Stack>
   );
 }
